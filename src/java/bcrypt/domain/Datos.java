d125bf3524494070e34a22699b04a93c6d358589
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2018-08-30 20:03:29 +0000

    nueva generacion de salt

--- src/java/bcrypt/domain/Datos.java
+++ src/java/bcrypt/domain/Datos.java
@@ -1,6 +1,7 @@
 
 package bcrypt.domain;
 
+import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.TreeMap;
@@ -12,14 +13,17 @@ import org.mindrot.jbcrypt.BCrypt;
  */
 public class Datos {
     private static List<TreeMap<String,Usuario>> datosUser = new ArrayList();
-
-     public static void insertar(Usuario usuario){
+    private static final String SALT = "salt";
+     
+    public static void insertar(Usuario usuario){
          TreeMap<String,Usuario> tm = new TreeMap<>();
-         String hashed = BCrypt.hashpw(usuario.getPassword(), BCrypt.gensalt());
+         //String hashed = BCrypt.hashpw(usuario.getPassword(), BCrypt.gensalt());
+         String hashed = BCrypt.hashpw(usuario.getPassword(), BCrypt.gensalt(12, new SecureRandom(SALT.getBytes())));
          System.out.println("cryt > "+hashed+" longitud "+hashed.length());
          usuario.setPassword(hashed);
          tm.put(usuario.getEmail(), usuario);
          datosUser.add(tm);
+    
      }
      
      public static Usuario consultar(Usuario usuario){
